e39cbf4eb85f447a47703c7494d42a3b5cd0bb8b
myproject2025 <167327944+myproject2025@users.noreply.github.com>
2024-10-18 11:16:01 +0500

    UPDATE

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -10,19 +10,24 @@ const About = () => {
       <Image
         alt="ecommerce"
          className=" lg:w-96 w-full  lg:h-auto h-64 object-cover object-center rounded-lg"
-        src={pic}
-      
-      
-      />
+        src={pic}/>
       <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
      
       
-        <p className="leading-relaxed">
-        Based out of Kannauj in Uttar Pradesh (India), Attar Perfume is one of the oldest attar perfume manufacturers in India. We have been the industry's pioneer and oldest company in India for more than 50 years. We started with a goal to provide the best quality natural fragrances to the world. Every fragrance we sell has undergone quality checks, is 100% authentic and premium, and has a long-lasting fragrance. We are known for our high-quality Indian attar perfumes all over the world. 
-        <br />
-        <br />
-        Our dedication to producing high-quality fragrances with our client's needs in mind has helped us make a name for ourselves in the business on a national and international level. We manufacture, supply, and export a wide range of attar perfumes to more than 50 nations, including Saudi Arabia (Dubai, Riyadh, Daman), Australia, Germany, France, and more. This presence is proof that we are globally competitive through our expertise in manufacturing superior quality fragrances.
-       </p>
+        <p className="leading-relaxed text-3xl font-bold mb-4">"Treasure of Fragrances!</p>
+
+ <p>Explore our website for the finest attar from around the world, crafted to enrich your life with exquisite scents. Our collection features:</p>
+<br />
+<p> - Pure and natural attar</p>
+ 
+ <p> - Unique and exclusive fragrances </p>
+<p> - High-quality and long-lasting scents </p>
+<p> - Affordable and competitive prices </p>
+<br />
+<p>Order your favorite attar online and we'll deliver it right to your doorstep."</p>
+
+
+       
       </div>
     </div>
   </div>
